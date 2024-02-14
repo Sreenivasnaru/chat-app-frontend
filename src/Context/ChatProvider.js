f5765689fdbbf1bdcfc4c815c1cd98e98f5b351c
Sreenivasnaru <Sreenunaru672@gmail.com>
2024-02-14 16:08:57 +0530

    Added Notification Feature

--- src/Context/ChatProvider.js
+++ src/Context/ChatProvider.js
@@ -7,6 +7,7 @@ const ChatProvider = ({ children }) => {
   const [user, setUser] = useState();
   const [selectedChat, setSelectedChat] = useState();
   const [chats, setChats] = useState([]);
+  const [notification, setNotification] = useState([]);
 
   const history = useHistory();
 
@@ -21,7 +22,16 @@ const ChatProvider = ({ children }) => {
 
   return (
     <ChatContext.Provider
-      value={{ user, setUser, setSelectedChat, selectedChat, chats, setChats }}
+      value={{
+        user,
+        setUser,
+        setSelectedChat,
+        selectedChat,
+        chats,
+        setChats,
+        notification,
+        setNotification,
+      }}
     >
       {children}
     </ChatContext.Provider>
